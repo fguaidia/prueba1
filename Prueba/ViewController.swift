614111515009dfd6f1972c834e44ae387f1595d9
Fabio Andres Guaidia Ruiz <fabioguaidia@hotmail.com>
2016-04-11 15:04:04 -0500

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Fabio Andres Guaidia Ruiz on 11/04/16.
+//  Copyright © 2016 Fabio Andres Guaidia Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
